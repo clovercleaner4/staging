e4712ec26b6de7a5f669cf53d7e9bdf69f85f51e
Vivid-BASE <showtimelabel@gmail.com>
2026-02-13 12:31:45 +0900

    Update GAS URL to new deployment

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -31,7 +31,7 @@ const Contact = () => {
         const data = new FormData(form);
 
         // GAS Payload
-        const gasUrl = 'https://script.google.com/macros/s/AKfycbwQK4sZm3ctXWcLUyaKb0OXAgXyn2Dx5tjT_fTMJv16pZYhDULsRIks6TAlB44P3Xkn/exec';
+        const gasUrl = 'https://script.google.com/macros/s/AKfycbw7F0Kr1wyW-dITdq3E6FMz19biXqadNwlQfkNjnktfZ-S4wJ0EhM7TDWhGF9izva5X/exec';
         const payload = {
             name: data.get('name'),
             email: data.get('email'),
