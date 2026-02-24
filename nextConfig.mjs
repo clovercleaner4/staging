346fe848e6e92ac56b5eebe03789181115f19ca7
Vivid-BASE <showtimelabel@gmail.com>
2026-02-24 21:12:35 +0900

    Emergency Restore: Correcting Production Site on Main Branch

---
+++ nextConfig.mjs
@@ -0,0 +1,10 @@
+const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
+    images: {
+        unoptimized: true,
+    },
+    productionBrowserSourceMaps: false,
+};
+
+export default nextConfig;
